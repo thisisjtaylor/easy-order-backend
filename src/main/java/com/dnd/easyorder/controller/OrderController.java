9bf3b0ad5346fac481bd83f3d610474b07d137ec
thisisjtaylor <thisisjtaylor@gmail.com>
2026-09-24 23:01:16 -0500

    Add configurable CORS and environment-based database config

--- src/main/java/com/dnd/easyorder/controller/OrderController.java
+++ src/main/java/com/dnd/easyorder/controller/OrderController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/order-service")
 public class OrderController {
